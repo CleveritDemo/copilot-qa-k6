5e9159a71fb9c27f02d6a15ad6a73818ee86383e
Contacto Traid <contacto@traid.com.ar>
2025-06-25 20:53:53 -0300

    Refactor performance test scripts to use centralized data and URLs

--- performance/tests/smoke-test.js
+++ performance/tests/smoke-test.js
@@ -1,11 +1,12 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { getTestOptions } from '../config/config-test-options.js';
+import { urls } from '../data/data-test.js';
 
 export const options = getTestOptions('smoke');
 
 export default function () {
-    const res = http.get('https://test.k6.io');
+    const res = http.get(urls.smokeTestUrl);
     check(res, {
         'status is 200': (r) => r.status === 200, // validate response code 200
         'response time is less than 250ms': (r) => r.timings.duration < 250, // response time less than 250ms
--- performance/tests/spike-test.js
+++ performance/tests/spike-test.js
@@ -1,28 +1,19 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { randomIntBetween, randomString } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 import { getTestOptions } from '../config/config-test-options.js';
+import { urls, getTestData } from '../data/data-test.js';
 
 export const options = getTestOptions('spike');
 
 export default function () {
-    const userId = randomIntBetween(1, 100);
-    const title = randomString(10);
-    const body = randomString(50);
-
-    const payload = JSON.stringify({
-        userId: userId,
-        title: title,
-        body: body,
-    });
-
+    const payload = getTestData();
     const params = {
         headers: {
             'Content-Type': 'application/json',
         },
     };
 
-    const res = http.post('https://jsonplaceholder.typicode.com/posts', payload, params);
+    const res = http.post(urls.spikeTestUrl, payload, params);
 
     check(res, {
         'status is 201': (r) => r.status === 201, // validate response code 201
